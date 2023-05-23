efe7ef2b9de4c1dee0b8c3832890064e5644f594
Evehic <116957692+Evehic@users.noreply.github.com>
2023-05-23 14:42:18 +0100

    Update route.js

--- app/getenv/route.js
+++ app/getenv/route.js
@@ -1,8 +1,9 @@
 import { NextResponse } from 'next/server';
+import dotenv from 'dotenv';
 
 export async function GET() {
- // var myenv= await process.env.MY
-  var myenv = "Hardcoded"
+  var myenv= await process.env.MY
+ // var myenv = "Hardcoded"
  // import dotenv from 'dotenv';
   return NextResponse.json({ msg: myenv});
  }
